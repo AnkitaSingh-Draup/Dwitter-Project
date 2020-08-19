2c9fa87b43092b6cb94e6ddfdf8e10ee40d0521d
Ankita <ankita.singh@draup.com>
2020-08-19 19:10:41 +0530

    postgres setup

--- dwitter/settings.py
+++ dwitter/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'dwitter_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'dwitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dwitter_1',
+        'USER': 'postgres',
+        'PASSWORD': 'draup@123',
+        'HOST': 'localhost'
     }
 }
 
